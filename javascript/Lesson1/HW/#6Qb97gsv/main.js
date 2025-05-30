8b44ad3863e8221f31c6b96415cde74b7372c85c
OneStedY <trofimovartem321@gmail.com>
2025-05-30 17:53:33 +0300

    Javascript, изменен реп, Дз 1 javascript

---
+++ javascript/Lesson1/HW/#6Qb97gsv/main.js
@@ -0,0 +1,7 @@
+{
+    let firstName = `Artem`
+    let middleName = `Trofymov`
+    let lastName = `Yuriovich`
+    let person = `${firstName} ${middleName} ${lastName}`
+    console.log(person)
+}
